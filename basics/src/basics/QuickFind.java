579fe9ba6f22cea022c708a29680c5d5c6802e35
aysedemirel <aysedemirel1996@gmail.com>
2021-08-25 23:02:12 +0300

    [NEW] Add quick find test class.

---
+++ basics/src/basics/QuickFind.java
@@ -0,0 +1,38 @@
+package basics;
+
+public class QuickFind {
+  private int[] id;
+
+  public QuickFind(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++)
+      id[i] = i;
+  }
+
+  public boolean connected(int p, int q) {
+    return id[p] == id[q];
+  }
+
+  public void union(int p, int q) {
+    int pid = id[p];
+    int qid = id[q];
+    for (int i = 0; i < id.length; i++)
+      if (id[i] == pid)
+        id[i] = qid;
+  }
+
+  public static void main(String[] args) {
+    final int sizeOfIdArr = 10;
+    QuickFind quickFind = new QuickFind(sizeOfIdArr);
+    quickFind.union(4, 3);
+    quickFind.union(3, 8);
+    quickFind.union(6, 5);
+    quickFind.union(9, 4);
+    quickFind.union(2, 1);
+    System.out.println("isConnected 8-9: " + quickFind.connected(8, 9));
+    System.out.println("isConnected 5-0: " + quickFind.connected(5, 0));
+    quickFind.union(5, 0);
+    quickFind.union(7, 2);
+    quickFind.union(6, 1);
+  }
+}
